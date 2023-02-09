70318b089ba3bf2d6b94969d44d37211a1e7a896
VinuthaTJ179 <93989675+VinuthaTJ179@users.noreply.github.com>
2023-02-09 17:08:37 +0530

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-        val = "Updation successful! Thank you for signing up, your details include: Movie ID: {0}, Movie Title: {1}, Movie Year: {2}, Movie Language: {3}, Director ID: {4}, Duration: {5}".format(mov_id,mov_title,mov_year,mov_lang,dir_id,duration)
+        val = "Updation successful! Thank you for signing up, your details include: Movie Title: {0}, Movie Year: {1}, Movie Language: {2}, Director ID: {3}, Duration: {4}".format(mov_title,mov_year,mov_lang,dir_id,duration)
